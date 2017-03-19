455b4d0ce48cf0180c2ec29df89fde9e0b136987
lukas <lukas.grasse@uleth.ca>
2017-03-18 19:25:31 -0600

    kinda works

--- main.cpp
+++ main.cpp
@@ -15,18 +15,32 @@ Landscape landscape(0.0f, 102.0f, 102);
 
 
 void draw(void) {
-    // User defined init
-
-
     // Black background
     glClearColor(0.0f,0.0f,0.0f,1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    landscape.drawCityGround();
+
+    // User defined init
+    glFrustum(-10.0, 110.0, -10.0, 110.0, -10, 100.0);
+
+    gluLookAt(0, 5, 0, 50, 0, 50, 1, 0, 0);
+
+
 
 
     glutSwapBuffers();
 }
 
-
+void reshape(int w, int h)
+{
+    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glFrustum(-10.0, 110.0, -10.0, 110.0, -5.0, 200.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
 
 //Main program
 
@@ -51,8 +65,9 @@ int main(int argc, char **argv) {
     //Call to the drawing function
     glutDisplayFunc(draw);
 
+    glutReshapeFunc(& reshape);
 
-    landscape.drawCityGround();
+
     
     // Loop require by OpenGL
     glutMainLoop();
