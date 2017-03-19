d2b2c7584a88dd57ece45169e98c06bd5c9113e5
lukas <lukas.grasse@uleth.ca>
2017-03-18 19:05:17 -0600

    added all classes

---
+++ Building.h
@@ -0,0 +1,14 @@
+//
+// Created by Lukas Grasse on 2017-03-18.
+//
+
+#ifndef HW_OPENGL_BUILDING_H
+#define HW_OPENGL_BUILDING_H
+
+
+class Building {
+
+};
+
+
+#endif //HW_OPENGL_BUILDING_H
---
+++ BuildingFactory.h
@@ -0,0 +1,14 @@
+//
+// Created by Lukas Grasse on 2017-03-18.
+//
+
+#ifndef HW_OPENGL_BUILDINGFACTORY_H
+#define HW_OPENGL_BUILDINGFACTORY_H
+
+
+class BuildingFactory {
+
+};
+
+
+#endif //HW_OPENGL_BUILDINGFACTORY_H
