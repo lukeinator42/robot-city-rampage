65f3238b387c956b450bf74bb34f0dd8cf0c4a32
Austin <kothiga2112@gmail.com>
2017-03-25 16:33:50 -0600

    added more key functionality. added documentation in main

--- main.cpp
+++ main.cpp
@@ -31,70 +31,154 @@ float botX = 0.f;
 float botY = 0.f;
 float botZ = 0.f;
 
-
 int rotNorm = 0;
 float rot = 0.f;
 float pos[2][4] = { {0.f, -1.f,  0.f, 1.f},
 		    {1.f,  0.f, -1.f, 0.f} };
 
-void myClick(int button, int state, int x, int y){}
+/* PAUSE */
+bool PAUSEBOOL = true;
 
+//
+// turns the robot based on
+// a specified direction
+// true  - left
+// false - right
+//
 void turn(bool direction)
 {
+   // control direction
    float dir = -1;
-   if(direction)
-      dir = 1;
-   
+   if(direction){ dir = 1; }  
+
+   // if the bot is in the center
+   // of a street it can turn
    if((int)botX % 6 == 0 && (int)botZ % 6 == 0)
-   {
-      // NOTE
-      // should be 90 in post
-      rot += (dir*90);
-      rotNorm = (rotNorm + (int)dir) % 4;
-   }
+   { rot += (dir*90); rotNorm = (rotNorm + (int)dir) % 4; }
 }
 
+//
+// reset the robot to center
+//
+void origin()
+{
+   EyeX =  0.f; LaX = 0.f; botX = 0.f; rot = 0.f;
+   EyeY = 1.5f; LaY = 0.f; botY = 0.f; rotNorm = 0;
+   EyeZ = -2.f; LaZ = 0.f; botZ = 0.f; 
+}
 
+//
+// Adds functionality for the following keys
+// p - pause the game
+// a - turn robot right
+// q - turn robot left
+// r - move robot back to center
+// z - push the robot forward
+//
 void myCBKey(unsigned char key, int x, int y)
 {
-   if(key == 122) /* z - push forward*/
-   {
-      botX += pos[0][rotNorm];
-      botZ += pos[1][rotNorm];
-      
-      EyeX += pos[0][rotNorm];
-      EyeZ += pos[1][rotNorm];
+   if(key == 112) { PAUSEBOOL = !PAUSEBOOL; }
 
-      LaX += pos[0][rotNorm];
-      LaZ += pos[1][rotNorm];
-   }
+   if(PAUSEBOOL)
+   {
+      if(key == 122) /* z - push forward */
+      {
+	 botX += pos[0][rotNorm];
+	 botZ += pos[1][rotNorm];
+	 
+	 EyeX += pos[0][rotNorm];
+	 EyeZ += pos[1][rotNorm];
+
+	 LaX += pos[0][rotNorm];
+	 LaZ += pos[1][rotNorm];
+      }
    
-   else if(key == 113){ turn(true);  } /* q - turn left*/
-   else if(key ==  97){ turn(false); } /* a - turn right*/
-   else
-      std::cout << "No function for key: " << key << "\n";  
+      else if(key == 113){ turn(true);  } /* q - turn left        */
+      else if(key ==  97){ turn(false); } /* a - turn right       */
+      else if(key == 114){ origin();    } /* r - return to origin */
+      else
+	 std::cout << "No function for key: " << key << "\n";
+   }
 }
 
-void draw() {
+//
+// Adds functionality for the following keys
+// F1 - turn head forward
+// F2 - turn head right
+// F3 - turn head left
+// F4 - move the camera back to default
+// F5 - move the camera to view 1
+// F6 - move the camera to view 2
+// F7 - move the camera to view 3
+// F8 - move the camera to view 4
+// 
+void specialCBKey(int key, int x, int y)
+{
+   if(PAUSEBOOL)
+   {
+      /* Head Turning Commands */
+      if(key == GLUT_KEY_F1){ /*turn head forward*/; }
+      else if(key == GLUT_KEY_F2){ /*TURN HEAD RIGHT*/; }
+      else if(key == GLUT_KEY_F3){ /*TURN HEAD LEFT*/; }
+      
+      /* Look at Changing Commands */
+      else if(key == GLUT_KEY_F4){ /*CHANGE BACK TO DEFAULT */; }
+      else if(key == GLUT_KEY_F5){/*view 1*/;}
+      else if(key == GLUT_KEY_F6){/*view 2*/;}
+      else if(key == GLUT_KEY_F7){/*view 3*/;}
+      else if(key == GLUT_KEY_F8){/*view 4*/;}
+   }
+}
 
-    glClear(GL_COLOR_BUFFER_BIT);
+//
+// Returns the robots head when
+// F2 or F3 key is released
+//
+void specialUpCBKey(int key, int x, int y)
+{
+   if(PAUSEBOOL)
+   {
+      if(key == GLUT_KEY_F2 || key == GLUT_KEY_F3)
+      {
+	 /* turn head forward */;
+      }
+   }
+}
 
-    glMatrixMode(GL_MODELVIEW);
-    
-    /* clear all pixels  */
-    glClear (GL_COLOR_BUFFER_BIT);
+//
+// Building destruction on left clicks
+// 
+void myClick(int button, int state, int x, int y)
+{
+   if(PAUSEBOOL)
+   {
+      
+   }
+}
+
+//
+// Main Drawing done here
+// 
+void draw()
+{
+
+   /* Clear buffer and set backdrop color */
+   glClear(GL_COLOR_BUFFER_BIT);
+   glClearColor(0.1, 0.39, 0.88, 1.0);
+   glMatrixMode(GL_MODELVIEW);
 
     /* Clear the current matrix */
     glLoadIdentity();
 
     /* Viewing transformation */
-    gluLookAt(EyeX, EyeY, EyeZ,  /*  Eye  */
-              LaX, LaY, LaZ,    /* Look at */
-              0.0, 1.0, 0.0);  /* Up vector */
+    gluLookAt(EyeX, EyeY, EyeZ, /*    Eye    */
+              LaX, LaY, LaZ,    /*  Look at  */
+              0.0, 1.0, 0.0);   /* Up vector */
 
-
+    /* Draw the landscaps */
     landscape.drawCityGround();
 
+    /* Draw the buildings */
     buildingFactory.draw();
     
     /* temp robot */
@@ -149,8 +233,10 @@ int main(int argc, char** argv) {
     //Call to the drawing function
     glutDisplayFunc(& draw);
     glutIdleFunc(& draw);
-    glutMouseFunc(&myClick);
-    glutKeyboardFunc(&myCBKey);
+    glutMouseFunc(& myClick);
+    glutKeyboardFunc(& myCBKey);
+    glutSpecialFunc(& specialCBKey);
+    glutSpecialUpFunc(& specialUpCBKey);
     glutReshapeFunc(& reshape);
 
     // Loop require by OpenGL
